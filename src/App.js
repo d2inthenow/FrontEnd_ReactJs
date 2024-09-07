7ee6b26629b8658e81ad02d47851154ff5c4ba49
duong\DUONG <duomg0023@gmail.com>
2024-09-07 12:21:13 +0700

    Hide Show

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
 import Home from './components/Home';
-
-// function App() {
+import AddNewProduct from './components/AddNewProduct';
+//JSX
+// function App() { 
 const App = () => {
-  let x = 5;
+
+  const x = [1, 2, 3, { name: 'duong' }]
+  // const x = { name: 'eric' }
+
   return (
-    <div className="App" style={
-      { border: '50px solid red', }
-    }>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App">
+      <header className="App-header content-left">
+        <div style={{ textAlign: "center" }}> <img src={logo} className="App-logo" alt="logo" /></div>
+
         <p>
-          Hello world ReactJs with x = {JSON.stringify(x)}
-          {console.log("check x >>>>: ", x)}
+          Hello world React with x = {JSON.stringify(x)}
+          {console.log('values x = ', x)}
         </p>
         <Home />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className='content-right'>
+        <AddNewProduct />
+      </div>
     </div>
   );
 }
 
-export default App;
 
+export default App;
---
+++ src/components/AddNewProduct.js
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+
+const AddNewProduct = () => {
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState(0);
+    const [size, setSize] = useState(0);
+    const [color, setColor] = useState("");
+    const [isShowDetail, setisShowDetail] = useState(true);
+
+    const handleClickBtn = () => {
+        let object = {
+            name: name,
+            price: price,
+            size: size,
+            color: color
+        };
+        console.log(">>> check data: ", object);
+    }
+
+    const handleHideShow = (status) => {
+        setisShowDetail(status)
+    }
+    return (
+        <div>
+            {
+                isShowDetail === true &&
+                <fieldset >
+                    <legend>Add A New Product</legend>
+                    <div className="input-product">
+                        <label>Name:</label>
+                        <input value={name} type="text" onChange={(event) => setName(event.target.value)} />
+                    </div>
+                    <div className="input-product">
+                        <label>Price:</label>
+                        <input value={price} type="text" onChange={(event) => setPrice(event.target.value)} />
+                    </div>
+                    <div className="input-product">
+                        <label>Size:</label>
+                        <input value={size} type="text" onChange={(event) => setSize(event.target.value)} />
+                    </div>
+                    <div className="input-product">
+                        <label>Color:</label>
+                        <input value={color} type="text" onChange={(event) => setColor(event.target.value)} />
+                    </div>
+                    <div>
+                        <button onClick={() => handleClickBtn()}>+Add New</button>
+                    </div>
+                </fieldset>
+            }
+            {/* {
+                isShowDetail === true && <div onClick={() => handleHideShow(false)}>Hide this form</div>
+            }
+
+            {
+                isShowDetail === false && <div onClick={() => handleHideShow(true)}>Show form</div>
+            } */}
+            {
+                isShowDetail === true ?
+                    <div onClick={() => handleHideShow(false)}>Hide this form</div> :
+                    <div onClick={() => handleHideShow(true)}>Show form</div>
+            }
+        </div>
+    )
+}
+
+export default AddNewProduct;
