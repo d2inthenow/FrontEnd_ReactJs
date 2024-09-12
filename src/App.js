2d0e6e69a01f063b2f20f7eb8f1a32419edffb57
duong\DUONG <duomg0023@gmail.com>
2024-09-12 16:56:18 +0700

    chuyen canh img & dieu huong trang

--- src/App.js
+++ src/App.js
@@ -3,36 +3,61 @@ import './App.scss';
 import Home from './components/Home';
 import AddNewProduct from './components/AddNewProduct';
 import Product from './components/Products/Product';
-import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
 import Nav from './components/Navigation/Nav';
-//JSX
-// function App() { 
+
+
+import {
+  BrowserRouter as Router, Switch, Route,
+} from "react-router-dom";
+
+
+
 const App = () => {
 
-  const x = [1, 2, 3, { name: 'duong' }]
+  // const x = [1, 2, 3, { name: 'duong' }]
   // const x = { name: 'eric' }
 
-  return (
-    <div>
-      <Nav />
-      <div className="App">
-        <header className="App-header content-left">
-          <div style={{ textAlign: "center" }}> <img src={logo} className="App-logo" alt="logo" /></div>
 
-          <p>
-            Hello world React with x = {JSON.stringify(x)}
-            {console.log('values x = ', x)}
-          </p>
-          <Home />
-        </header>
-        <div className='content-right'>
-          <AddNewProduct />
-          <hr />
+  return (
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/" exact={true}>
+          <div className="App">
+            <header className="App-header content-left">
+              <div style={{ textAlign: "center" }}> <img src={logo} className="App-logo" alt="logo" /></div>
+
+              <p>
+                <span>Hello world React</span> with Duong
+
+              </p>
+              <Home />
+            </header>
+            <div className='content-right'>
+              <AddNewProduct />
+              <hr />
+              <Product />
+            </div>
+          </div>
+        </Route>
+        <Route path="/product">
           <Product />
-        </div>
-      </div>
-    </div>
+        </Route>
+        <Route path="/weather">
+          <div>weather app</div>
+        </Route>
+        <Route path="/about">
+          <div>2iamd2</div>
+        </Route>
+
+        <Router path="*">
+          <div>404 NOT FOUND</div>
+        </Router>
+      </Switch>
+
+    </Router>
+
   );
 }
 
